6bf2725ce9b91beaa6b174e04c97f6f6570384fe
willlldan <173040071@mail.unpas.ac.id>
2022-02-05 14:01:43 +0700

    Basic Utility Part 1 - Spacing, Sizing and Typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./public/**/*.{html, js}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '13': '3.25rem',
+        '15': '3.75rem',
+        '128': '32rem',
+        '144': '36rem',
+      },
+      fontFamily: {
+        roboto: ['Roboto']
+      }
+    },
   },
   plugins: [],
 }
